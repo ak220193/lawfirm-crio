3bbd224041d6f342c279afc9d584f10bdc05c3be
ak220193 <mail2meak22frcrio@gmail.com>
2024-01-28 12:10:48 +0530

    Folder Structure intail commit

---
+++ src/Pages/Mainpage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Mainpage = () => {
+  return <div>TEst</div>;
+};
+
+export default Mainpage;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Mainpage from "./Pages/Mainpage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Mainpage />,
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<RouterProvider router={router} />);
