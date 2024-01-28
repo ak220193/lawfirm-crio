3f9adf512857c35f7b0f6b11efcecb01e069211b
ak220193 <mail2meak22frcrio@gmail.com>
2024-01-28 21:53:58 +0530

    changes needs to be done

---
+++ src/Components/BannerText/BannerText.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./BannerText.module.css";
+import { Box } from "@mui/material";
+
+const BannerText = () => {
+  return (
+    <div className={styles.BannerText}>
+      <p>Let’s Introduce Ourself</p>
+    </div>
+  );
+};
+
+export default BannerText;
--- src/Pages/Mainpage.jsx
+++ src/Pages/Mainpage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../Components/Navbar/Navbar";
 import Herobanner from "../Components/Herobanner/Herobanner";
+import BannerText from "../Components/BannerText/BannerText";
 
 const Mainpage = () => {
   return (
     <div>
       <Navbar />
       <Herobanner />
+      <BannerText />
     </div>
   );
 };
