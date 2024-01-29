af10af5056653ad42bc88fa57018de1da74f065e
ak220193 <mail2meak22frcrio@gmail.com>
2024-01-29 11:17:23 +0530

    bug

---
+++ src/Components/ChooseUs/ChooseUs.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./Chooseus.module.css";
+
+function ChooseUs() {
+  return (
+    <>
+      <div className={styles.sectioncard}>
+        <h1> Why Choose Us ? </h1>
+      </div>
+    </>
+  );
+}
+
+export default ChooseUs;
--- src/Pages/Mainpage.jsx
+++ src/Pages/Mainpage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../Components/Navbar/Navbar";
 import Herobanner from "../Components/Herobanner/Herobanner";
 import BannerText from "../Components/BannerText/BannerText";
+import ChooseUs from "../Components/ChooseUs/ChooseUs";
 
 const Mainpage = () => {
   return (
@@ -9,6 +10,7 @@ const Mainpage = () => {
       <Navbar />
       <Herobanner />
       <BannerText />
+      <ChooseUs />
     </div>
   );
 };
